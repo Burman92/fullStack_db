38824ea8730dd8f61dff3ba477f535a07a2fde10
Burman Bedford <burman@Burmans-Air.lan>
2023-03-03 18:40:53 -0500

    attempt 3 to deploy

--- migration.js
+++ migration.js
@@ -1,22 +1,23 @@
 const { Pool } = require('pg');
 const DATABASE_URL = process.env.DATABASE_URL;
 const pool = require('./dbConn');
+pool.query(`DROP TABLE IF EXISTS characters`);
 /*----------------CREATING A VILLAGEs TABLE-------------------- */
 pool.query(`CREATE TABLE IF NOT EXISTS village (
-    id int PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name text)`, (error, data)=>{
         if(error){
             console.log("CREATE TABLE village failed");
         } else {
+            console.log(data)
             console.log("village table created")
         }
     }
     )
 /*-----------------CREATING A CHARACTER TABLE----------------- */
-pool.query(`DROP TABLE IF EXISTS characters`);
 pool.query(`CREATE TABLE IF NOT EXISTS characters (
     id SERIAL PRIMARY KEY,
-    name text
+    name text,
     villages_id int NOT NULL
     FOREIGN KEY(village_id) 
     REFERENCES village.id)`, (error, data)=>{
